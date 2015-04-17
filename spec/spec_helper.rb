1079f01a2c59379437bc65c529e097fe15f8acb1
Yu Hoshino <yhoshino11@gmail.com>
2015-04-18 00:13:55 +0900

    prepare for coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+require 'coveralls'
+Coveralls.wear!
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yhoshino11_todo'
