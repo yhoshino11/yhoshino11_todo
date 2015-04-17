87a25b6cadb52c37c38efb8fc89c66883c942980
Yu Hoshino <yhoshino11@gmail.com>
2015-04-18 01:43:00 +0900

    execute database connection before test

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-ActiveRecord::Base.establish_connection adapter: 'sqlite3',database: ':memory:'
 describe Yhoshino11Todo::DB do
   context 'initial state' do
     let(:database) { Yhoshino11Todo::DB.prepare }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,11 @@ CodeClimate::TestReporter.start
 require 'coveralls'
 Coveralls.wear!
 
-RSpec.configure do |config|
-  require 'factory_girl'
-  config.include FactoryGirl::Syntax::Methods
-end
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'yhoshino11_todo'
+
+RSpec.configure do |config|
+  require 'factory_girl'
+  ActiveRecord::Base.establish_connection adapter: 'sqlite3',database: ':memory:'
+  config.include FactoryGirl::Syntax::Methods
+end
