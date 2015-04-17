607f9819ed29d9d9a0f72b508c5b87d03d030dfd
Yu Hoshino <yhoshino11@gmail.com>
2015-04-17 17:36:07 +0900

    modified spec

--- spec/yhoshino11_todo_spec.rb
+++ spec/yhoshino11_todo_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Yhoshino11Todo do
-  let(:version) { Yhoshino11Todo::VERSION }
+  context 'version' do
+    let(:version) { Yhoshino11Todo::VERSION }
 
-  it 'has a version number' do
-    expect(version).not_to be nil
-  end
+    it 'has number' do
+      expect(version).not_to be nil
+    end
 
-  it 'is v0.0.1' do
-    expect(version).to eq('0.0.1')
+    it 'is v0.0.1' do
+      expect(version).to eq('0.0.1')
+    end
   end
 end
