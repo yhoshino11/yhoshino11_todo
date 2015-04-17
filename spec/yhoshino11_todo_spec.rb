b7e0a9a841af6f924dc1c5d2ac75865ba03e024c
Yu Hoshino <yhoshino11@gmail.com>
2015-04-17 16:56:23 +0900

    removed unneeded spec

--- spec/yhoshino11_todo_spec.rb
+++ spec/yhoshino11_todo_spec.rb
@@ -4,8 +4,4 @@ describe Yhoshino11Todo do
   it 'has a version number' do
     expect(Yhoshino11Todo::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
