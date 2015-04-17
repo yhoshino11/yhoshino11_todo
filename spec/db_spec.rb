24259894c80f4ab5f26dba6632aaf41d4a7a45b6
Yu Hoshino <yhoshino11@gmail.com>
2015-04-18 00:42:18 +0900

    modified to travis

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe Yhoshino11Todo::DB do
   context 'initial state' do
+    let(:database) { Yhoshino11Todo::DB.prepare }
     it 'is empty' do
-      expect(Yhoshino11Todo::DB.prepare).to be nil
+      expect(database).to be nil
     end
   end
 end
