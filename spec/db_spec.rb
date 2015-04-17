adf266a3e13e26e2438f07dfe83f3f9c6dc8a7e0
Yu Hoshino <yhoshino11@gmail.com>
2015-04-18 01:29:28 +0900

    execute database creation before test

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Yhoshino11Todo::DB do
   context 'initial state' do
+    before { Yhoshino11Todo::DB.prepare }
     let(:database) { Yhoshino11Todo::DB.prepare }
     it 'is empty' do
       expect(database).to be nil
