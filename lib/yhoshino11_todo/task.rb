efcdfa0a0e865ea896913ea24d464510ba605ab7
Yu Hoshino <yhoshino11@gmail.com>
2015-04-19 02:15:33 +0900

    add validation

--- lib/yhoshino11_todo/task.rb
+++ lib/yhoshino11_todo/task.rb
@@ -5,6 +5,7 @@ require 'active_record'
 module Yhoshino11Todo
   class Task < ActiveRecord::Base
     scope :status_is, ->(status) { where(status: status) }
+
     NOT_YET = 0
     DONE = 1
     PENDING = 2
@@ -14,5 +15,9 @@ module Yhoshino11Todo
       'DONE'    => DONE,
       'PENDING' => PENDING
     }.freeze
+
+    validates :name,    presence: true,     length: { maximum: 140 }
+    validates :content, presence: true
+    validates :status,  numericality: true, inclusion: { in: STATUS.values }
   end
 end
--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,16 +1,33 @@
 require 'spec_helper'
 
 describe Yhoshino11Todo::DB do
+  before { Yhoshino11Todo::DB.prepare }
+
   context 'initial state' do
-    before { Yhoshino11Todo::DB.prepare }
     let(:database) { Yhoshino11Todo::DB.prepare }
     it 'is empty' do
       expect(database).to eq nil
     end
+  end
+  it 'is CRUD' do
+    task = create(:task)
+    expect(Yhoshino11Todo::Task.count).to eq 1
+  end
 
-    it 'is CRUD' do
-      task = create(:task)
-      expect(Yhoshino11Todo::Task.count).to eq 1
+  context 'validates' do
+    it 'with nil' do
+      task = build(:task, name: nil, content: nil)
+      expect(task).not_to be_valid
+    end
+
+    it 'max length of name is 140' do
+      task = build(:task, name: 'a'*141)
+      expect(task).not_to be_valid
+    end
+
+    it 'status' do
+      task = build(:task, status: 'DOING')
+      expect(task).not_to be_valid
     end
   end
 end
