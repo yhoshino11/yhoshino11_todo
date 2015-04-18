c31854aa4c5e113eb8b77c58f3da6f80603e6d16
Yu Hoshino <yhoshino11@gmail.com>
2015-04-19 05:06:52 +0900

    modified command/options spec

--- spec/command/options_spec.rb
+++ spec/command/options_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Yhoshino11Todo::Command do
+describe Yhoshino11Todo::Command::Options do
   context 'from command line' do
-    let(:command) { Yhoshino11Todo::Command }
+    let(:option) { Yhoshino11Todo::Command::Options }
     it 'shows version' do
       action = `bundle exec ruby bin/yhoshino11_todo -v`
       current_version = Yhoshino11Todo::VERSION
@@ -11,28 +11,28 @@ describe Yhoshino11Todo::Command do
 
     context 'allows options' do
       it 'to create' do
-        options = command::Options.parse!(['create', '-n name_val'])
+        options = option.parse!(['create', '-n name_val'])
         expect(options).to eq({ command: 'create', name: ' name_val' })
       end
 
       it 'to search' do
-        options = command::Options.parse!(['search', '-s done'])
+        options = option.parse!(['search', '-s done'])
         expect(options).to eq({ command: 'search', status: ' done' })
       end
 
       context 'to update' do
         it 'name' do
-          options = command::Options.parse!(['update', '-n another_name'])
+          options = option.parse!(['update', '-n another_name'])
           expect(options).to eq({ command: 'update', name: ' another_name' })
         end
 
         it 'content' do
-          options = command::Options.parse!(['update', '-c another_content'])
+          options = option.parse!(['update', '-c another_content'])
           expect(options).to eq({ command: 'update', content: ' another_content' })
         end
 
         it 'status' do
-          options = command::Options.parse!(['update', '-s pending'])
+          options = option.parse!(['update', '-s pending'])
           expect(options).to eq({ command: 'update', status: ' pending' })
         end
       end
