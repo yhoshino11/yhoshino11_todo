83c1e3f4850fc79812334e1fb02ac68ed649f77a
Yu Hoshino <yhoshino11@gmail.com>
2015-04-19 01:17:38 +0900

    task can be created with command

--- lib/yhoshino11_todo/command.rb
+++ lib/yhoshino11_todo/command.rb
@@ -2,5 +2,12 @@
 
 module Yhoshino11Todo
   class Command
+    def execute
+      DB.prepare
+    end
+
+    def create_task(name, content)
+      Task.create!(name: name, content: content).reload
+    end
   end
 end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper.rb'
 
 describe Yhoshino11Todo::Command do
+  let(:command) { Yhoshino11Todo::Command.new }
+  let(:tasks) { Yhoshino11Todo::Task }
+  let(:task) { build(:task) }
+
+  context 'initial state' do
+    it 'is nil' do
+      expect(command.execute).to be nil
+    end
+  end
+
+  context 'allows' do
+    it 'to create' do
+      command.create_task(task[:name], task[:content])
+      expect(tasks.count).to eq(1)
+    end
+
+    it 'to search' do
+      expect(tasks.where(name: task[:name]).first.id).to eq(1)
+    end
+  end
 end
