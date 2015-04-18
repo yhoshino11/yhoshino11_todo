7c5fa7ba42b80b37b07bbe1e40b79e1317217b21
Yu Hoshino <yhoshino11@gmail.com>
2015-04-19 04:24:16 +0900

    task can be created from command line (before refactoring)

--- lib/yhoshino11_todo/command/options.rb
+++ lib/yhoshino11_todo/command/options.rb
@@ -6,6 +6,17 @@ module Yhoshino11Todo
   class Command
     module Options
       def self.parse!(argv)
+        options = {}
+
+        sub_command_parsers = Hash.new do |key,val|
+          raise ArgumentError, "'#{val}' is not yhoshino11_todo sub command."
+        end
+
+        sub_command_parsers['create'] = OptionParser.new do |opt|
+          opt.on('-n VAL', '--name=VAL', 'task name') { |val| options[:name] = val }
+          opt.on('-c VAL', '--content=VAL', 'task content') { |val| options[:content] = val }
+        end
+
         command_parser = OptionParser.new do |opt|
           opt.on_head('-v', '--version', 'Show program version') do |v|
             opt.version = Yhoshino11Todo::VERSION
@@ -15,7 +26,15 @@ module Yhoshino11Todo
           end
         end
 
-        command_parser.parse!(argv)
+        begin
+          command_parser.order!(argv)
+          options[:command] = argv.shift
+          sub_command_parsers[options[:command]].parse!(argv)
+        rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
+          abort e.message
+        end
+
+        options
       end
     end
   end
--- spec/command_options_spec.rb
+++ spec/command_options_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe Yhoshino11Todo::Command do
-  let(:command) { Yhoshino11Todo::Command }
-  it 'shows version' do
-    action = `bundle exec ruby bin/yhoshino11_todo -v`
-    current_version = Yhoshino11Todo::VERSION
-    expect(action).to eq("yhoshino11_todo #{current_version}\n")
+  context 'from command line' do
+    let(:command) { Yhoshino11Todo::Command }
+    it 'shows version' do
+      action = `bundle exec ruby bin/yhoshino11_todo -v`
+      current_version = Yhoshino11Todo::VERSION
+      expect(action).to eq("yhoshino11_todo #{current_version}\n")
+    end
+
+    it 'allows options' do
+      options = command::Options.parse!(['create', '-n name_val'])
+      expect(options).to eq({ command: 'create', name: ' name_val' })
+    end
   end
 end
